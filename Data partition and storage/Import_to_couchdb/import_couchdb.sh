20a8f41c615813bfac654ffcb7f631037921fbdc
Tiangewang0524 <31025791+Tiangewang0524@users.noreply.github.com>
2018-10-24 17:16:32 +1100

    Add files via upload

---
+++ Data partition and storage/Import_to_couchdb/import_couchdb.sh
@@ -0,0 +1,60 @@
+#!/bin/bash 
+# path="/vol1/o0DictData"
+# for f in `ls $path/*.json`
+# do 
+# echo $f;
+# curl -X PUT -d @$f http://admin:admin@115.146.85.95:5984/backup_db/${f};
+# done
+
+curl -X PUT -d @"/vol1/o0DictData/o0Data0.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_00
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data1.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_01
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data2.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_02
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data3.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_03
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data4.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_04
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data5.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_05
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data6.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_06
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data7.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_07
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data8.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_08
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data9.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_09
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data10.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_10
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data11.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_11
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data12.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_12
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data13.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_13
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data14.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_14
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data15.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_15
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data16.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_16
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data17.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_17
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data18.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_18
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data19.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_19
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data20.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_20
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data21.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_21
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data22.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_22
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data23.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_23
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data24.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_24
+echo "Task done"
+curl -X PUT -d @"/vol1/o0DictData/o0Data25.json" http://admin:admin@115.146.85.95:5984/backup_db/doc_25
+echo "Task done"
